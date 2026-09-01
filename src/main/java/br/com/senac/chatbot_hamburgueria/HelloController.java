d6779bec6297fd56df586250bcb695ee986abf7d
Fernando Maciel <fernando.maciel@prof.sc.senac.br>
2026-09-01 19:23:13 -0300

    feat: Commit inicial

---
+++ src/main/java/br/com/senac/chatbot_hamburgueria/HelloController.java
@@ -0,0 +1,35 @@
+package br.com.senac.chatbot_hamburgueria;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    private final ChatClient chatClient;
+
+    public HelloController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    record ChatHelloInput(
+            String prompt
+    ){}
+
+    record ChatHelloOutput(
+            String message
+    ){}
+
+    @PostMapping
+    @Operation(summary = "Envia uma mensagem para o Gemini")
+    public ChatHelloOutput hello(@RequestBody ChatHelloInput input) {
+        String chatResponse = chatClient.prompt()
+                .user(input.prompt)
+                .call()
+                .content();
+
+        return new ChatHelloOutput(chatResponse);
+    }
+}
